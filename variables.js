ae49228380fc7170b1852b11b1b3574d2f4fdae4
season89890 <seasonzuruson@yahoo.com>
2025-08-12 03:35:22 +0100

    first commit

---
+++ variables.js
@@ -0,0 +1,45 @@
+
+console.log("Hello wworld");
+
+console.log("Season Nwazuru");
+console.log(90);
+//JAVASCRIPT VARIABLES - X=3 Y=2
+//Declare variables - const let
+const age = 60;
+// age = 78; error
+console.log(age);
+
+let y = 90;
+y=100;
+console.log(y);
+
+//rules of naming variables
+//1. variables can not start with numbers, %
+// 2. reserved keywords (const let, if )
+//const let = 89
+//3. you can not have spaces in variables
+// let my name = 'john'
+
+//conventions when naming 
+//1. Use logical and meanungful names
+//2. compound names e.g my name account Number my address
+// camelcasing
+
+const accountNumber =78747744
+const myCountry = 'Togo'
+
+//determine if the var is going to const or let
+
+const firstName = 'Season' 
+const countryOfBirth = 'Nigeria'
+const minimumEligibleVotingAge = 18;
+
+//DATA TYPES
+// simple (promitive) and complex (non-primitive)
+//strings, numbers, null, boolean and undefined
+
+// complex arrays, objects
+
+
+
+
